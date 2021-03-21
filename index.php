311488dc4d2360e5808ad317c0198091d904a01e
runacode <59107019+runacode@users.noreply.github.com>
2021-03-21 12:58:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php $company = "SHERRY HILL PAINTING LLC";
-$phone = "(631) 968-6669";
-	$address1 = "1102 QUEENS PLZ S";
-	$address2 = "LONG ISLAND CITY, NY 11101";
+<?php $company = "BEYOND SALON & NAILS";
+$phone = "(732) 389-1234";
+	$address1 = "226 STATE ROUTE 35";
+	$address2 = "EATONTOWN, NJ 07724";
 ?>
 <!DOCTYPE html>
 <html style="font-size: 16px;">
